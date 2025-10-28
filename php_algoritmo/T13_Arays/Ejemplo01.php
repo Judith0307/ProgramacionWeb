59b6ba99a10f969a2172ceea93138a285d45f28e
eva <judith030307.gmail.com>
2025-10-28 07:59:31 -0500

    Modificado

--- php_algoritmo/T13_Arays/Ejemplo01.php
+++ php_algoritmo/T13_Arays/Ejemplo01.php
@@ -47,3 +47,27 @@ foreach ($numeros as $num) {
     echo $num . "\n";
 }
 ?>
+
+<?php
+$numeros = array(10, 25.5, 30, 42.75);
+
+//Mostramos valores individuales del arreglo
+echo "Ejemplo 1: Primer número: " . $numeros[0] . "\n";
+echo "Ejemplo 2: Segundo número: " . $numeros[1] . "\n";
+echo "Ejemplo 3: Tercer numero: ". $numeros[2] . "\n";
+echo "Ejemplo 4: Cuarto numero: ". $numeros[3] . "\n";
+
+//Caluculamos la suma de todos los elementos 
+$suma = array_sum($numeros);
+
+//Contar cuantos elementos hay 
+$cantidad = count($numeros);
+
+//Calcular el promedio 
+$promedio = $suma / $cantidad;
+
+//Mostrar resultados
+echo "\nSuma total: " . $suma . "\n";
+echo "Cantidad de elementos: " . $cantidad . "\n";
+echo "Promedio: " . number_format($promedio, 2) . "\n";
+?>
